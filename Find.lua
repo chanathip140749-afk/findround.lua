caafd5e220877a2d715d6634088dae16f80d9bfb
chanathip140749-afk <chanathip140749@gmail.com>
2026-04-22 23:37:05 +0700

    Update Find.lua

--- Find.lua
+++ Find.lua
@@ -1,1 +1,17 @@
-# findround.lua
+local player = game.Players.LocalPlayer
+local playerGui = player:WaitForChild("PlayerGui")
+local roundEnd = playerGui:WaitForChild("RoundEnd")
+local voteRemote = game:GetService("ReplicatedStorage"):WaitForChild("Systems"):WaitForChild("Voting"):WaitForChild("Vote")
+local function sendRetry()
+    local args = {"Next"}
+    voteRemote:FireServer(unpack(args))
+end
+roundEnd:GetPropertyChangedSignal("Enabled"):Connect(function()
+    if roundEnd.Enabled == true then
+        task.wait(1) 
+        while roundEnd.Enabled == true do
+            sendRetry()
+            task.wait(1) 
+        end
+    end
+end)
